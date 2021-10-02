58089ae20585b0d48d235ab3d2622a6871cc24d6
olafhichwa <ooh5406@rit.edu>
2021-10-02 13:05:21 -0400

    fixed errors so we can compile

--- src/main/java/com/webcheckers/util/SignInActions.java
+++ src/main/java/com/webcheckers/util/SignInActions.java
@@ -1,9 +1,18 @@
 package com.webcheckers.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SignInActions {
-    List<Obj> list = new ArrayList<Obj> ();
+    List<String> NamesInUse = new ArrayList<String>();
+
+
+    public boolean isNameValid(String Name){
+        //I enter my name containing at least one alphanumeric character and optionally spaces
+        return false;
+    }
     public boolean isNameUnique(String Name){
-        if (Name.) //todo check if name is unique
+        //if (Name.) //todo check if name is unique
 
         return false;
     }
