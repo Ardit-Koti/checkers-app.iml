2e5c4d3b3a374a9fd33025461cac7d6a79291396
olafhichwa <ooh5406@rit.edu>
2021-10-02 14:13:17 -0400

    updated player class and created a new player

--- src/main/java/com/webcheckers/model/Player.java
+++ src/main/java/com/webcheckers/model/Player.java
@@ -1,5 +1,18 @@
 package com.webcheckers.model;
 
 public class Player {
+
+    public Player(String player_name) {
+        this.player_name = player_name;
+    }
+
     private String player_name;
+
+    public String getPlayer_name() {
+        return player_name;
+    }
+
+    public void setPlayer_name(String player_name) {
+        this.player_name = player_name;
+    }
 }
--- src/main/java/com/webcheckers/util/PlayerLobby.java
+++ src/main/java/com/webcheckers/util/PlayerLobby.java
@@ -1,27 +1,30 @@
 package com.webcheckers.util;
 
+import com.webcheckers.model.Player;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class SignInActions {
-    List<String> NamesInUse = new ArrayList<String>(); //
+public class PlayerLobby {
+    List<String> NamesInUse = new ArrayList<>(); //
 
     /**
-     * @return String error message if check fails and name is not added, return "T" if done
+     * @return String error message if check fails and name is not added, return "Success" if done
      */
     public String checkAndAddName(String Name){
-        //todo checks
-        if (! this.isNameUnique(Name)) //if the name fails uniqueness check, return false
-            return "The name '"+Name+"' is already taken, please enter a new name";
+        //Checks
         if (! isNameValid(Name)) //if the name fails validity check, return false
             return "The name '"+Name+"' is invalid, please insure it contains at least one alphanumeric character, please enter a new name";
+        if (! this.isNameUnique(Name)) //if the name fails uniqueness check, return false
+            return "The name '"+Name+"' is already taken, please enter a new name";
         //if we get here, name valid
-        //todo add
+        //addition
         NamesInUse.add(Name);
-        return "T";
+        new Player(Name);
+        return "Success";
     }
 
-    // TODO: 10/2/21 remove a name after sign out
+    // TODO: 10/2/21 remove a name after sign out (not in sprint 1)
 
     public boolean isNameValid(String Name){
         if (!this.containsAlphanumeric(Name) ) //if name does NOT contain alphanumeric chars, return false,
