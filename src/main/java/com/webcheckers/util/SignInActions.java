582dd2bd99d185b5c0bc98f77a02536c15cfed93
olafhichwa <ooh5406@rit.edu>
2021-10-02 12:49:05 -0400

    created new ftl and made progress on user name processing class

---
+++ src/main/java/com/webcheckers/util/SignInActions.java
@@ -0,0 +1,11 @@
+package com.webcheckers.util;
+
+public class SignInActions {
+    List<Obj> list = new ArrayList<Obj> ();
+    public boolean isNameUnique(String Name){
+        if (Name.) //todo check if name is unique
+
+        return false;
+    }
+
+}
