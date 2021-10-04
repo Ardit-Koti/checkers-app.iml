bfa79fc5423617c68b906aaadc1ed4dbef1d5d2f
Ardit <ak1935@rit.edu>
2021-10-04 17:31:55 -0400

    board constr.

--- src/main/java/com/webcheckers/model/Board.java
+++ src/main/java/com/webcheckers/model/Board.java
@@ -3,6 +3,9 @@ package com.webcheckers.model;
 public class Board {
     private Space[][] gameboard;
 
+    public Board(){
+    }
+
     public Space getSpace(int row, int col){
         return gameboard[row][col];
     }
