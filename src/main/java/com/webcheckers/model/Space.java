eafa79c730875b4547dbf5444c21dfd09be3779e
BenG <gurevichbenjamin@gmail.com>
2021-10-19 16:04:59 -0400

    Board and Piece

--- src/main/java/com/webcheckers/model/Space.java
+++ src/main/java/com/webcheckers/model/Space.java
@@ -1,20 +1,35 @@
 package com.webcheckers.model;
 
 public class Space {
-    public Space(){
-
-    }
     private boolean isTaken;
     public enum shade {light, dark}
     private shade shade;
+    private Piece piece;
+
+
+    public Space(){
+        this.isTaken = false;
+        this.piece = piece;
+    }
+
 
     public void setShade(Space.shade shade) {
         this.shade = shade;
     }
+
     public void moveTo(){
         this.isTaken=true;
     }
+
     public void moveAway(){
         this.isTaken=false;
+    }
+
+    public boolean isTaken() {
+        return isTaken;
+    }
+
+    public void setTaken(boolean valid_square) {
+        this.isTaken = valid_square;
     }
 }
