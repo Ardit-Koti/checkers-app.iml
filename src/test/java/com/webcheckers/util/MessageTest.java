bfc6ff45c8c710f13e2265e905bfd07799649d79
Ardit <ak1935@rit.edu>
2021-10-20 17:18:01 -0400

    Message and PostHomeRoute Tests

--- src/test/java/com/webcheckers/util/MessageTest.java
+++ src/test/java/com/webcheckers/util/MessageTest.java
@@ -1,4 +1,44 @@
 package com.webcheckers.util;
+import spark.*;
+import org.junit.jupiter.api.*;
 
+import javax.sound.sampled.EnumControl;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
+import java.util.logging.Logger;
+
+/**
+ * Author: Ardit Koti
+ */
+@Tag("util-tier")
 public class MessageTest {
+    private Message CuT;
+
+    @BeforeEach
+    void setup()
+    {
+        CuT = mock(Message.class);
+    }
+
+    @Test
+    void errorTest()
+    {
+        Message m = Message.error(CuT.getText());
+        Assertions.assertSame(Message.Type.ERROR, m.getType());
+    }
+
+    @Test
+    void infoTest()
+    {
+        Message m = Message.info(CuT.getText());
+        Assertions.assertSame(Message.Type.INFO, m.getType());
+    }
+
+    @Test
+    void toStringTest()
+    {
+        String s_rep = CuT.toString();
+        Assertions.assertSame("{Msg " + CuT.getType() + " '" + CuT.getText() + "'}" , s_rep );
+    }
 }
