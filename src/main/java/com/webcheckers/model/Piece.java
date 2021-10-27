95eaf67b72c9875a7140d4afc0612bc360ba9c39
Ryan Wolford <rcw3446@rit.edu>
2021-10-27 14:37:05 -0400

    Added some state functionality to the Piece class

--- src/main/java/com/webcheckers/model/Piece.java
+++ src/main/java/com/webcheckers/model/Piece.java
@@ -22,16 +22,21 @@ public class Piece {
 
     private final type variety;
     private final color color;
+    private State state;
 
 
     public Piece(type variety, color color) {
         this.variety = variety;
         this.color = color;
+        this.state = State.Alive;
 
     }
 
+    public void killPiece(){this.state = State.Dead;}
     public type getType(){return this.variety;}
     public color getColor(){return this.color;}
+    public State getState(){return this.state;}
+
 
 }
 
