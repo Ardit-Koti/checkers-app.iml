3d7396676c3b3580f8f223381a56c2ffc3439800
Ryan Wolford <rcw3446@rit.edu>
2021-10-27 15:11:15 -0400

    Added tests for the color of the player and its accessor. Fixed some errors with the other tests due to the changes made to the Player class instructors. Fixed misused assert statements.

--- src/test/java/com/webcheckers/model/PieceTest.java
+++ src/test/java/com/webcheckers/model/PieceTest.java
@@ -4,10 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Author: Ryan Wolford
+ * This is a test class for the model tier class Piece
+ */
 @Tag("Model-Tier")
 class PieceTest {
     private Piece CuT;
 
+    /**
+     * Tests the ability to correctly get the Type of the piece for each of its potential values
+     */
     @Test
     public void testGetType(){
         CuT = new Piece(Piece.type.KING, Piece.color.Red);
@@ -16,6 +23,9 @@ class PieceTest {
         assertEquals(Piece.type.REGULAR, CuT.getType());
     }
 
+    /**
+     * Tests the ability to correctly get the Color of the piece for each of its potential values
+     */
     @Test
     public void testGetColor(){
         CuT = new Piece(Piece.type.KING, Piece.color.Red);
@@ -24,6 +34,10 @@ class PieceTest {
         assertEquals(Piece.color.White, CuT.getColor());
     }
 
+    /**
+     * Tests the ability to correctly get the State of the piece as well as the functionality of the
+     * killPiece() function in the Piece class.
+     */
     @Test
     public void testState(){
         CuT = new Piece(Piece.type.KING, Piece.color.Red);
