23dfcf3a24388f55574609619737a733dd9cccc4
olafhichwa <ooh5406@rit.edu>
2021-11-11 21:01:30 -0500

    updated is over in game to check for each color

--- src/main/java/com/webcheckers/model/Game.java
+++ src/main/java/com/webcheckers/model/Game.java
@@ -16,6 +16,8 @@ public class Game {
 
     private Player whitePlayer;
 
+    private Color winner;
+
     private com.webcheckers.model.Color Color;
 
     private int gameId = 1; // TODO: 10/6/21  implement number generator for multiple games
@@ -47,29 +49,48 @@ public class Game {
     public static final String JUMP_AGAIN = "You must jump again";
     public static final String ERROR_INVALID_MOVE = "ERROR: invalid move.";
 
-    /**
-     * Lets the player take his or her turn.
-     *
-     * @param move a move that the player is making.
-     * @return a string with the results of the attempt to make the move.
-     */
 
     public boolean isOver(){
         // check if all pieces are dead
+        int redPieces = 0;
+        int whitePieces = 0;
+
         Iterator<Row> gameOverRowIterator = gameBoard.iterator();
         for (int i = 0; i < 8; i++) {
             Row tempRow = gameOverRowIterator.next();
             for (int j = 0; j < 8; j++) {
                 Iterator<Space> gameOverSpaceIterator = tempRow.iterator();
                 Space tempSpace = gameOverSpaceIterator.next();
-                if (tempSpace.hasPiece())
-                    return true;
+                if (tempSpace.hasPiece()){
+                    if(tempSpace.piece.getColor().equals(com.webcheckers.model.Color.RED))
+                        redPieces++;
+                    if(tempSpace.piece.getColor().equals(com.webcheckers.model.Color.WHITE))
+                        whitePieces++;
+                }
+
             }
         }
+
+        if (redPieces == 0 ){ //white one
+            winner = com.webcheckers.model.Color.WHITE;
+            return true;
+        }
+
+        if (whitePieces==0){
+            winner = com.webcheckers.model.Color.RED;
+        }
+
 
         //todo check if no possible moves exist
         return false;
     }
+
+    /**
+     * Lets the player take his or her turn.
+     *
+     * @param move a move that the player is making.
+     * @return a string with the results of the attempt to make the move.
+     */
     public String takeTurn(Move move) {
         if(currentMove.isEmpty()) {
             boolean isValid = validator.isMoveValid(move);
