7cb0d6ba6eb047d079a29131cf0824dbb75cb4fe
olafhichwa <ooh5406@rit.edu>
2021-11-11 19:55:17 -0500

    final commit before switching to ryans branch

--- src/main/java/com/webcheckers/ui/PostValidateMove.java
+++ src/main/java/com/webcheckers/ui/PostValidateMove.java
@@ -11,7 +11,16 @@ import spark.Route;
 import spark.TemplateEngine;
 import spark.Session;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PostValidateMove implements Route{
+
+
+    static final String NAME_PARAM = "name";
+    private final String VIEW_MODE = "viewMode";
+    private final String USER = "currentUser";
+    private final String CHOSEN_PLAYER = "challenge";
 
     private final TemplateEngine templateEngine;
     private final PlayerLobby pLobby;
@@ -25,8 +34,29 @@ public class PostValidateMove implements Route{
 
     @Override
     public Object handle(Request request, Response response) throws Exception {
+
+
+        // getting session information and the proper game
+        final Session httpSession = request.session();
+        final Map<String, Object> vm = new HashMap<>();
+        httpSession.attribute(GetHomeRoute.PLAYER_LOBBY, pLobby);
+        httpSession.attribute(NAME_PARAM, NAME_PARAM);
+        vm.put(VIEW_MODE, ViewMode.PLAY); // Need to pass in 3 players
+
+        Player youPlayer = httpSession.attribute(USER); // originally "YOU"
+        final String opponentPlayerName = request.queryParams(CHOSEN_PLAYER);
+
+
+        //todo
+
+
+
+
         Gson gson = new Gson(); //todo make gson object global using webserver, similar to template engine
         Move move = gson.fromJson(request.queryParams("actionData"), Move.class);
+
+        //go player -> game
+
 
         System.out.println("Move info:: \n    " +
                 "start: (" + move.getStartPos().getCell()+ ","+ move.getStartPos().getRow() + ")"+"\n    " +
