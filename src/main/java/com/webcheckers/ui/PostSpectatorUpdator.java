f679bafc7a3803150cfe9f9513d2f3da5a635526
Ryan Wolford <rcw3446@rit.edu>
2021-11-15 20:56:45 -0500

    Added a route to update the spectator's color with the active color for board rendering

---
+++ src/main/java/com/webcheckers/ui/PostSpectatorUpdator.java
@@ -0,0 +1,51 @@
+package com.webcheckers.ui;
+
+import com.google.gson.Gson;
+import com.webcheckers.application.GameCenter;
+import com.webcheckers.model.Color;
+import com.webcheckers.model.Game;
+import com.webcheckers.model.Player;
+import com.webcheckers.util.Message;
+import spark.*;
+
+import java.util.Objects;
+
+public class PostSpectatorUpdator implements Route{
+
+    final static String JOINED_MESS = "You are spectating successfully";
+
+
+    private final GameCenter gameCenter;
+
+    private Gson gson;
+
+    private final TemplateEngine templateEngine;
+
+    public PostSpectatorUpdator(Gson gson, GameCenter gameCenter,TemplateEngine templateEngine) {
+        this.gameCenter = gameCenter;
+        this.templateEngine = templateEngine;
+        this.gson = gson;
+    }
+
+    @Override
+    public String handle(Request request, Response response) {
+        final Session httpSession = request.session();
+        final Player user = httpSession.attribute("currentUser");
+
+        Game game = user.getGame();
+        Color currentColor = game.getActiveColor();
+
+        if(user.getColor() == null){
+            user.setColor(currentColor);
+            return gson.toJson(Message.info(JOINED_MESS));
+        }
+        else if(user.getColor().equals(currentColor)){
+            return gson.toJson(Message.info("false"));
+        }
+        else{
+            user.setColor(currentColor);
+            return gson.toJson(Message.info("true"));
+        }
+
+    }
+}
